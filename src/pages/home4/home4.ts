8ea821a85de9ec2f7396aa8789e054c4288f278d
nattasamphoangoen <natta151515@gmail.com>
2018-07-02 03:10:38 +0700

    add and update

--- src/pages/home4/home4.ts
+++ src/pages/home4/home4.ts
@@ -6,8 +6,10 @@ import { AngularFireDatabase } from 'angularfire2/database';
   templateUrl: 'home4.html'
 })
 export class HomePage4 {
+
   
-  public led1 = "false";
+  public led1 = 0;
+  public led2 = 0;
   
   constructor(public navCtrl: NavController,  private db: AngularFireDatabase) {}
 
@@ -15,6 +17,11 @@ export class HomePage4 {
     console.log(this.led1);
     const itemRef = this.db.object('Home4/led');
     itemRef.update({led1:this.led1});
+  }
+  led2Switch(){
+    console.log(this.led2);
+    const itemRef = this.db.object('Home4/led');
+    itemRef.update({led2:this.led2});
   }
 
 
